cd25a0b3e6076a539606f790d4267f4e14396386
Brigitte Padilla <mbpadilla4@misena.edu.co>
2023-10-17 09:33:57 -0500

    Desarrollo del componente empleados.components.ts

--- frontend/src/app/components/empleados/empleados.component.ts
+++ frontend/src/app/components/empleados/empleados.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Empleado } from 'src/app/models/empleado';
+import { EmpleadoService } from "../../services/empleado.service";
+
+declare var M: any;
 
 @Component({
   selector: 'app-empleados',
   templateUrl: './empleados.component.html',
-  styleUrls: ['./empleados.component.scss']
+  styleUrls: ['./empleados.component.scss'],
+  providers: [EmpleadoService]
 })
-export class EmpleadosComponent {
-
-}
+export class EmpleadosComponent implements OnInit
+{
+  constructor(public empleadoService:
+    EmpleadoService) { }
+    ngOnInit(): void {
+  }
+  
+  agregarEmpleado(form?: NgForm) {
+    this.empleadoService.PostEmpleado(form?.value)
+    .subscribe(res => {
+    this.resetForm(form);
+    M.toast({html: 'Guardado satisfactoriamente'});
+    });
+  
+  }
+  resetForm(form?: NgForm) { // Limpiar el formulario, recibe un formulario como parametro
+    if (form) {
+    form.reset();
+    this.empleadoService.selectedEmpleado = new Empleado();
+    }
+    }
+    }
